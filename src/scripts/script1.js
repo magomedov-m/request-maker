5a60930dcf7061ffad966e1a9cb80c6ecda53ff1
Магомед Магомедов <128976763+magomedov-m@users.noreply.github.com>
2025-04-17 10:50:01 +0300

    Сделал заявление для вступления в профсоюз

--- src/scripts/script1.js
+++ src/scripts/script1.js
@@ -1,6 +1,21 @@
 'use strict'
 
-const doc = new jsPDF();
+let buttonDownload = document.querySelector('.download')
+buttonDownload.addEventListener('click', generateText);
 
-doc.text('это тестовые текст', 10, 10);
-doc.save("a4.pdf");
+function generateText() {
+    const {jsPDF} = window.jspdf;
+    const doc = new jsPDF();
+
+    doc.setFont("helvetica");
+    doc.text('Заявление о вступлении в профсоюз', 10, 10);
+    doc.text('Я, ФИО, дата рождения, проживающий по адресу: адрес,', 10, 20);
+    doc.text('работаю в название организации на должности должность,', 10, 30);
+    doc.text('прошу принять меня в члены первичной профсоюзной организации.', 10, 40);
+    doc.text('Согласен(на) с уставом профсоюза и обязуюсь выполнять его требования.', 10, 50);
+    doc.text('Дата: дата', 10, 60);
+    doc.text('Подпись: ____________________', 10, 70);
+
+            // Сохранение PDF
+    doc.save('заявление_о_вступлении_в_профсоюз.pdf');
+}
